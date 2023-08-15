a047d71b8339a36c802a71a36b0a0275dcb3c8ff
p3nguin-kun <p3nguinkun@proton.me>
2023-08-15 22:05:14 +0700

    update

---
+++ ports/guake/set_colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# Save this script into set_colors.sh, make this file executable and run it: 
+# 
+# $ chmod +x set_colors.sh 
+# $ ./set_colors.sh 
+# 
+# Alternatively copy lines below directly into your shell. 
+
+gconftool-2 -s -t string /apps/guake/style/background/color '#101010101010'
+gconftool-2 -s -t string /apps/guake/style/font/color '#f8f8f8f8f6f6'
+gconftool-2 -s -t string /apps/guake/style/font/palette '#080808080808:#ffff89898989:#f2f2ffff8a8a:#fefefafa6767:#a9a97878f6f6:#fdfd5656f9f9:#8a8afefeffff:#fefefefefefe:#101010101010:#ffff98989898:#f7f7ffff9f9f:#fefeffff5757:#adaddedef8f8:#fefe7878ffff:#8a8affffffff:#ffffffffffff'
---
+++ ports/linux-tty/macaroni-dark.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ "$TERM" = "linux" ]; then
+  /bin/echo -e "
+  \e]P0080808
+  \e]P1ff8989
+  \e]P2f2ff8a
+  \e]P3fefa67
+  \e]P4a978f6
+  \e]P5fd56f9
+  \e]P68afeff
+  \e]P7fefefe
+  \e]P8101010
+  \e]P9ff9898
+  \e]PAf7ff9f
+  \e]PBfeff57
+  \e]PCaddef8
+  \e]PDfe78ff
+  \e]PE8affff
+  \e]PFffffff
+  "
+  # get rid of artifacts
+  clear
+fi
